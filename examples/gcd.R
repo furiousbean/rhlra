be69c28257dc4955d2d2700e814646d83d7d2cc5
Nikita Zvonarev <nikitazvonarev@gmail.com>
2019-10-08 03:12:22 +0300

    Updated example & help

--- examples/gcd.R
+++ examples/gcd.R
@@ -6,3 +6,33 @@ print(x$gcd)
 
 x <- hlra_sylvester(list(c(1, -3, 3, -1), c(1, -2, 1)), 2)
 print(x$gcd)
+
+library(fftw)
+
+ssa_convolve <- function(u, v) {
+    p <- planFFT(length(u) + length(v) - 1)
+    l_fft <- FFT(c(u, numeric(length(v) - 1)), plan = p)
+    r_fft <- FFT(c(v, numeric(length(u) - 1)), plan = p)
+    Re(IFFT(l_fft * r_fft, plan = p))
+}
+
+set.seed(15)
+
+x <- rnorm(10)
+y <- rnorm(20)
+z <- rnorm(30)
+
+x <- x/sqrt(sum(x^2))
+
+sd <- 0.01
+
+first_poly <- ssa_convolve(x, y) + rnorm(29) * sd
+second_poly <- ssa_convolve(x, z) + rnorm(39) * sd
+
+r <- length(x) - 1
+result <- hlra_sylvester(list(first_poly, second_poly), r, debug = T,
+                         additional_pars = list(cadzow_scheme_order = 0,
+                                                cadzow_it_limit = 20))
+
+print(x)
+print(result$gcd)
